399a3f12a859086c464653309375d1158f084a43
SealtielFreak <Die.Seal_Valderrama@live.com>
2023-03-30 13:07:44 -0600

    Upload hardware and software testing for SDL2

--- ext/include/sapphire.h
+++ ext/include/sapphire.h
@@ -1,6 +1,10 @@
 #ifndef SAPPHIRE_SAPPHIRE_H
 #define SAPPHIRE_SAPPHIRE_H
 
-void hello(void);
+#define SDL_SOFTWARE_MODE       0
+#define SDL_HARDWARE_MODE       1
+#define SDL_GL_MODE             2
+
+int run_sdl2_test(int mode);
 
 #endif //SAPPHIRE_SAPPHIRE_H
--- ext/sapphire.c
+++ ext/sapphire.c
@@ -5,6 +5,9 @@
 #include "SDL2/SDL.h"
 #include "ruby.h"
 
+#define SCREEN_WIDTH_SDL2   640
+#define SCREEN_HEIGHT_SDL2  480
+
 /*
  * This function is for SDL2 testing.
  * Params:
@@ -16,24 +19,140 @@ VALUE rb_init_sdl2_test(VALUE self) {
         return Qtrue;
     }
 
-    if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         printf("SDL2 was init everything failure!\n");
         return Qfalse;
     }
 
     printf("SDL2 was init everything success!\n");
 
+    SDL_Quit();
+
     return Qtrue;
 }
 
-VALUE rb_hello(VALUE self) {
-    return Qnil;
+VALUE rb_run_sdl2_test(VALUE self, VALUE rb_mode) {
+    Check_Type(rb_mode, T_FIXNUM);
+
+    return INT2NUM(run_sdl2_test(NUM2INT(rb_mode)));
 }
 
 void Init_ext(void) {
     rb_define_global_function("init_sdl2_test", rb_init_sdl2_test, 0);
+    rb_define_global_function("run_sdl2_test", rb_run_sdl2_test, 1);
 }
 
-void hello(void) {
-    printf("Hello, World!\n");
+int run_sdl2_surface() {
+    int status = 1;
+    SDL_Window *window = NULL;
+    SDL_Surface *surface = NULL;
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    window = SDL_CreateWindow(
+            "SDL2 in Software",
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            SCREEN_WIDTH_SDL2, SCREEN_HEIGHT_SDL2,
+            SDL_WINDOW_SHOWN
+    );
+
+    if (window == NULL) {
+        fprintf(stderr, "could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    surface = SDL_GetWindowSurface(window);
+
+    while (status) {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
+                status = 0;
+            }
+        }
+
+        SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+
+        SDL_UpdateWindowSurface(window);
+    }
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+    return 0;
+}
+
+
+int run_sdl2_renderer() {
+    int status = 1;
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    window = SDL_CreateWindow(
+            "SDL2 in Hardware",
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            SCREEN_WIDTH_SDL2, SCREEN_HEIGHT_SDL2,
+            SDL_WINDOW_SHOWN
+    );
+
+    if (window == NULL) {
+        fprintf(stderr, "could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    
+    while (status) {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
+                status = 0;
+            }
+        }
+
+        int w, h;
+        SDL_GetWindowSize(window, &w, &h);
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+    return 0;
+}
+
+
+int run_sdl2_gl() {
+    return -1;
+}
+
+int run_sdl2_test(int mode) {
+    switch (mode) {
+        case SDL_SOFTWARE_MODE:
+            return run_sdl2_surface();
+        case SDL_HARDWARE_MODE:
+            return run_sdl2_renderer();
+        case SDL_GL_MODE:
+            return run_sdl2_gl();
+    }
+
+    return -1;
 }
